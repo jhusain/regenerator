b86a3d431c4fc279a6c7fd4c7876970715ccb20f
Ben Newman <bn@cs.stanford.edu>
2014-04-12 13:42:39 -0700

    Add a basic test of generator.return(argument);

--- test/tests.es6.js
+++ test/tests.es6.js
@@ -1631,3 +1631,31 @@ describe("for loop with var decl and no update expression", function() {
     check(range(), []);
   });
 });
+
+describe("generator return method", function() {
+  (runningInTranslation ? it : xit)
+  ("should behave as if generator actually returned", function() {
+    var executedFinally = false;
+
+    function *gen() {
+      try {
+        yield 0;
+      } catch (err) {
+        assert.ok(false, "should not have executed the catch handler");
+      } finally {
+        executedFinally = true;
+      }
+    }
+
+    var g = gen();
+    assert.deepEqual(g.next(), { value: 0, done: false });
+
+    assert.deepEqual(g.return("argument"), {
+      value: "argument",
+      done: true
+    });
+
+    assert.strictEqual(executedFinally, true);
+    assertAlreadyFinished(g);
+  });
+});
